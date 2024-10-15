ad0c725d137fa91e82c981cfcde730aac3a423a2
genryu1672 <146915000+genryu1672@users.noreply.github.com>
2024-10-15 13:51:57 +0900

    PG3_01_03

--- main.cpp
+++ main.cpp
@@ -3,19 +3,11 @@
 
 typedef void (*PFunc)(int*);
 
-int twice(int a) {
-	return a * 2;
-}
-
-int triple(int a) {
-	return a * 3;
-}
-
 //コールバック関数
 void DispResult(int* s) {
 
-	//printf("正解\n", *s);	
-	printf("正解\n", *s);
+	printf("正解\n", *s);	
+	//printf("正解\n", *s);
 }
 
 void setTimeOut(PFunc p, int second) {
@@ -31,8 +23,7 @@ int main() {
 	//関数ポインタを宣言
 	//void(*pfunc)();
 	
-	scanf_s("%d\n");//出力
-	
+	scanf_s("正解\n");//出力
 	//scanf_s("不正解\n");//出力
 	PFunc p;
 	p = DispResult;//ここで中身を入れる(実行する関数を切り替えることもできる)
